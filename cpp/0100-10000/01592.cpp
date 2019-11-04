6e95b3a81dedebd89ac2777524350a82f499612b
onsoim <onsoim@gmail.com>
2019-11-04 23:43:23 +0900

    01592 remove unnecessary code

--- cpp/0100-10000/01592.cpp
+++ cpp/0100-10000/01592.cpp
@@ -1,16 +1,5 @@
 #include <stdio.h>
 
-int gcd(int a, int b) {
-	int tmp = 0;
-
-	while (b != 0) {
-		tmp = a % b;
-		a = b, b = tmp;
-	}
-
-	return a;
-}
-
 int main() {
 	int N = 0, M = 0, L = 0;
 	int n[1000] = {0};
