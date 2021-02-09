7b2ed8040ee8983994360767af952adda6ec6d50
onsoim <onsoim@gmail.com>
2021-02-09 16:27:37 +0900

    [REFACTOR] project
    Ready for restart PS studying.

--- utils/MV_tc_TO_codeF.sh
+++
@@ -1,9 +0,0 @@
-cd testcase
-
-for Folder in $(ls)
-do
-    for FILE in $(ls "$Folder")
-    do
-        mv "$Folder/$FILE" "../solved/10001-20000/$Folder/"
-    done
-done
--- utils/auto_sync.sh
+++
@@ -1,4 +0,0 @@
-while true;do
-	python3 sync.py &
-	sleep 600
-done
