07967d3999dad6aad87c57b7cf5280fe6fb046dd
onsoim <onsoim@gmail.com>
2021-02-09 18:06:06 +0900

    [ADD] python 3 branch

--- Solved/10001-20000/10171/10171.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "\\    \/\\\n )  ( \'\)\n\(  \/  \)\n \\\(__\)|";
-    return 0;
-}
--- Unsolved/05373/TC/tc.py
+++
@@ -1,7 +0,0 @@
-import random
-
-count = random.randint(0,100)
-print(f"1\n{count}")
-for i in range(count):
-    print(random.choice(['U', 'D', 'F', 'B', 'R', 'L']) + random.choice(['+', '-']), end=" ")
-print()
