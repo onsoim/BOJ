f9e599465dbc15cc6af5eb0c4d8fd89c33890b7a
onsoim <onsoim@gmail.com>
2021-11-19 00:04:53 +0900

    [ADD] 09653 - 스타워즈 로고

---
+++ Solved/01000-09999/09653/solve.kt
@@ -0,0 +1,15 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    println("    8888888888  888    88888")
+    println("   88     88   88 88   88  88")
+    println("    8888  88  88   88  88888")
+    println("       88 88 888888888 88   88")
+    println("88888888  88 88     88 88    888888")
+    println("")
+    println("88  88  88   888    88888    888888")
+    println("88  88  88  88 88   88  88  88")
+    println("88 8888 88 88   88  88888    8888")
+    println(" 888  888 888888888 88  88      88")
+    println("  88  88  88     88 88   88888888")
+}
