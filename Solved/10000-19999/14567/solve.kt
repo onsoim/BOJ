b41ff8fc633c5e7c769531d0bfa0a8d9a47d6ac8
onsoim <onsoim@gmail.com>
2022-03-04 17:35:55 +0900

    [ADD] 14567: 선수과목 (Prerequisite)

---
+++ Solved/10000-19999/14567/solve.kt
@@ -0,0 +1,43 @@
+import java.io.*
+import java.util.*
+
+fun get(m: Int) = readLine()!!.split(" ").map { it.toInt() - m }
+
+fun Topological_sort(vertex: Array<ArrayList<Int>>, inDegree: Array<Int>, N: Int) {
+    val visited = Array(N, { 0 })
+    val queue = PriorityQueue<Pair<Int, Int>>(compareBy({ it.first }))
+
+    inDegree.forEachIndexed { i, v ->
+        if (v == 0) {
+            queue.offer(Pair(1, i))
+            visited[i] = 1
+        }
+    }
+
+    while (queue.isNotEmpty()) {
+        val (d, i) = queue.poll()
+
+        vertex[i].forEach {
+            if (--inDegree[it] == 0 && visited[it] == 0) {
+                queue.offer(Pair(d + 1, it))
+                visited[it] = d + 1
+            }
+        }
+    }
+
+    println(visited.joinToString(" "))
+}
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val (N, M) = get(0)
+    val vertex = Array(N, { arrayListOf<Int>() })
+    val inDegree = Array(N, { 0 })
+
+    repeat(M) {
+        val (A, B) = get(1)
+        vertex[A] += B
+        inDegree[B]++
+    }
+
+    Topological_sort(vertex, inDegree, N)
+}
