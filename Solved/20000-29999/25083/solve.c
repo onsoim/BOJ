a081dcc53721cd7e8963a62b9bc85c11f2f5b845
onsoim <onsoim@gmail.com>
2023-04-26 22:52:12 +0900

    [ADD] 25038: 새싹

---
+++ Solved/20000-29999/25083/solve.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |\n");
+    // printf("         ,r'\"7\n");
+    // printf("r`-_   ,'  ,/\n");
+    // printf(" \\. \". L_r'\n");
+    // printf("   `~\\/\n");
+    // printf("      |\n");
+    // printf("      |\n");
+
+    return 0;
+} 
